d30120018706fc5e18ce87583736b59e7c3e2aa7
AkaKitsune027 <kuronekosilent@gmail.com>
2024-09-27 23:19:47 +0700

    65130500070 - [Add] GameplayLayout

---
+++ src/views/CookingView.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <div class="bg-base h-screen">
+        <div class="bg-slate-50 flex gap-3 w-full p-3">
+            <button class="bg-primary w-12 rounded-lg">
+                <img src="/arrow-back.svg" class="w-10" />
+            </button>
+
+            <button class="bg-primary w-12 rounded-lg">
+                <img src="/book-open.svg" class="w-10" />
+            </button>
+
+            <button class="bg-primary w-12 rounded-lg">
+                <img src="/book-open.svg" class="w-10" />
+            </button>
+        </div>
+
+    </div>
+</template>
+
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'primary': '#ed9848',
+        'secondary': '#f1c83a',
+        'third': '#a15852',
+        'base': '#6f6f4b',
+        'alert': '#4a2b30',
+      }
+    }
   },
-  plugins: [require('daisyui')]
+  // plugins: [daisyui]
 }
 
