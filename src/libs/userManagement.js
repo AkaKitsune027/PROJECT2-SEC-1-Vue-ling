f7a4e059ccdd6c2885b083f41e5d49750fa79aa1
Rina <94218479+AkaKitsune027@users.noreply.github.com>
2024-10-05 21:49:43 +0700

    Update userManagement.js

--- src/libs/userManagement.js
+++ src/libs/userManagement.js
@@ -1,6 +1,6 @@
 const SERVER_URL = import.meta.env.VITE_SERVER_URL
 
-export async function getUserbyUserName(userN,ame) {
+export async function getUserbyUserName(userName) {
   const response = await fetch(SERVER_URL + `/game`)
   const data = await response.json()
   console.log(data)
