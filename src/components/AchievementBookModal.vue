fcdcbabe6d6bd1e10ce13d76f201b0ac63bfd735
AkaKitsune027 <kuronekosilent@gmail.com>
2024-10-19 03:01:23 +0700

    070 - [Edit, Fix] Fix bug and edit some UI and fix some bug function

--- src/components/AchievementBookModal.vue
+++ src/components/AchievementBookModal.vue
@@ -12,7 +12,7 @@ const selectedMenuIngredients = ref([]) // Store the selected menu ingredients
 
 // Map fiveStarMenus to corresponding food data
 const mappedFiveStarMenus = computed(() => {
-  return ?.userDetail?.fiveStarMenus.map((menu) => {
+  return userStore.user?.userDetail.fiveStarMenus.map((menu) => {
     const food = foodsData.find((food) => food.id === menu.foodId) // Assuming foods.json has an 'id' field
     // const ingredient = ingredientsData.find((ingredient) => ingredient.id === food.ingredients[0]
     // )
@@ -45,7 +45,7 @@ function showSelectedMenu(menu) {
           ingredients.push({
             type: ingredientName,
             name: ingredientName,
-            display_name: `${ingredientName === 'meat' ? 'เนื้อ' : 'ผัก'
+            display_name: `${ingredientName === "meat" ? "เนื้อ" : "ผัก"
               }อะไรก็ได้`,
             amount: 1,
             // image: `/meat/meat.png`,
@@ -84,106 +84,126 @@ function showSelectedMenu(menu) {
 
 <template>
   <!-- Modal -->
-  <div class="fixed inset-0 bg-gray-800 bg-opacity-70 flex justify-center items-center pl-10">
+  <div class="fixed inset-0 bg-gray-800 bg-opacity-70 flex justify-center items-center pl-20">
     <div class="relative w-full max-w-2xl max-h-full">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow">
         <!-- Modal header -->
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-          <h3 class="text-3xl font-bold text-gray-600">Achievement Book</h3>
-          <button @click="closeModal"
-            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-            </svg>
-          </button>
-        </div>
-        <!-- Modal body -->
-        <div class="body h-[630px] overflow-y-auto">
-          <div class="grid grid-cols-4 pt-2">
-            <!-- Empty div for the first grid column -->
-            <img src="/5stars.png" alt="banner" class="col-span-2 col-start-2 flex justify-center" />
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 class="text-3xl font-bold text-gray-600">Achievement Book</h3>
+            <button @click="closeModal"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                <button @click="closeModal"
+                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                  </svg>
+                </button>
           </div>
-          <div class="pt-0 p-8 flex flex-col gap-2 lg:grid grid-cols-3 grid-flow-row lg:gap-6">
-            <div @click="showSelectedMenu(menu)" v-for="(menu, index) in mappedFiveStarMenus" :key="index" :class="{
-              grayscale: !menu.isUnlock,
-              'hover:scale-[1.03] duration-300': menu.isUnlock,
-            }" class="gradient rounded-lg shadow-xl border-4 border-[#eed285] p-1 cursor-pointer">
-              <div v-if="menu.isUnlock">
-                <img :src="`/foods/${menu.name}.png`" alt="food.name"
-                  class="w-full h-auto drop-shadow-[0_8px_5px_rgba(0,0,0,0.3)]" />
-                <p class="text-center mt-1 font-semibold text-lg font-noto-thai text-gray-600">
-                  {{ menu.display_name }}
-                </p>
-              </div>
+          <!-- Modal body -->
+          <div class="body h-[630px] overflow-y-auto">
+            <div class="grid grid-cols-4 pt-2">
+              <!-- Empty div for the first grid column -->
+              <img src="/5stars.png" alt="banner" class="col-span-2 col-start-2 flex justify-center" />
+              <img src="/5stars.png" alt="banner" class="col-span-2 col-start-2 flex justify-center" />
+            </div>
+            <div class="pt-0 p-8 flex flex-col gap-2 lg:grid grid-cols-3 grid-flow-row lg:gap-6">
+              <div @click="showSelectedMenu(menu)" v-for="(menu, index) in mappedFiveStarMenus" :key="index" :class="{
+                grayscale: !menu.isUnlock,
+                'hover:scale-[1.03] duration-300': menu.isUnlock,
+              }" class="gradient rounded-lg shadow-xl border-4 border-[#eed285] p-1 cursor-pointer">
+                <div @click="showSelectedMenu(menu)" v-for="(menu, index) in mappedFiveStarMenus" :key="index" :class="{
+                  grayscale: !menu.isUnlock,
+                  'hover:scale-[1.03] duration-300': menu.isUnlock,
+                }" class="gradient rounded-lg shadow-xl border-4 border-[#eed285] p-1 cursor-pointer">
+                  <div v-if="menu.isUnlock">
+                    <img :src="`/foods/${menu.name}.png`" alt="food.name"
+                      class="w-full h-auto drop-shadow-[0_8px_5px_rgba(0,0,0,0.3)]" />
+                    <p class="text-center mt-1 font-semibold text-lg font-noto-thai text-gray-600">
+                      <img :src="`/foods/${menu.name}.png`" alt="food.name"
+                        class="w-full h-auto drop-shadow-[0_8px_5px_rgba(0,0,0,0.3)]" />
+                    <p class="text-center mt-1 font-semibold text-lg font-noto-thai text-gray-600">
+                      {{ menu.display_name }}
+                    </p>
+                  </div>
 
-              <div v-else class="opacity-55">
-                <img src="/unknownDish.png" alt="unknown-dish" class="w-full h-auto" />
-                <p class="text-center mt-1 font-semibold text-gray-600">
-                  Locked
-                </p>
+                  <div v-else class="opacity-55">
+                    <img src="/unknownDish.png" alt="unknown-dish" class="w-full h-auto" />
+                    <img src="/unknownDish.png" alt="unknown-dish" class="w-full h-auto" />
+                    <p class="text-center mt-1 font-semibold text-gray-600">
+                      Locked
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
 
-    <!-- Selected Menu -->
-    <div class="relative bg-white rounded-lg shadow w-[38rem] h-auto mx-10">
-      <div class="body p-4">
-        <h1 :class="{
-          'text-4xl font-bold text-gray-600 my-4 font-noto-thai text-center':
-            selectedMenu?.display_name,
-          'text-4xl font-bold text-gray-600 my-4 font-rowdies text-center':
-            !selectedMenu?.display_name,
-        }">
-          {{ selectedMenu?.display_name || 'Mystery Menu' }}
-        </h1>
-        <div class="flex justify-center items-center">
-          <img :src="selectedMenu
-            ? `/foods/${selectedMenu?.name}.png`
-            : '/unknownDish.png'
-            " :alt="selectedMenu?.name || 'unknown dish'" class="w-60" />
-        </div>
-        <div class="description px-12 py-2">
-          <p class="text-2xl font-bold text-gray-600">Description</p>
-          <p class="font-noto-thai font-medium text-gray-600 text-lg indent-6 ps-4 py-2">
-            {{
-              selectedMenu?.description ||
-              'Achieve a 5-star rating to collect all of these menus!'
-            }}
-          </p>
-        </div>
-        <div class="ingredients px-12 py-2">
-          <p class="text-2xl font-bold text-gray-600">Ingredients</p>
-          <div class="container flex flex-row justify-items-center gap-4 m-4 w-auto h-auto overflow-x-auto">
-            <div class="flex flex-col gap-2 mb-4 px-2 py-2 rounded-lg min-w-20 bg-gray-200" v-if="!selectedMenu"
-              v-for="n in 8" :key="n">
+        <!-- Selected Menu -->
+        <div class="relative bg-white rounded-lg shadow w-[38rem] h-auto mx-10">
+          <div class="body p-4">
+            <h1 :class="{
+              'text-4xl font-bold text-gray-600 my-4 font-noto-thai text-center':
+                selectedMenu?.display_name,
+              'text-4xl font-bold text-gray-600 my-4 font-rowdies text-center':
+                !selectedMenu?.display_name,
+            }">
+              {{ selectedMenu?.display_name || "Mystery Menu" }}
+            </h1>
+            <div class="flex justify-center items-center">
               <img :src="selectedMenu
-                ? `/meat/${selectedMenu?.ingredients}.png`
-                : '/unknownIngredient.png'
-                " :alt="selectedMenu?.ingredients || 'unknown ingredient'" />
-              <p class="text-sm text-center">Unknown Ingredient</p>
+                ? `/foods/${selectedMenu?.name}.png`
+                : '/unknownDish.png'
+                " :alt="selectedMenu?.name || 'unknown dish'" class="w-60" />
             </div>
-            <div class="flex flex-col gap-2 mb-4 px-2 py-2 rounded-lg min-w-20 bg-gray-200"
-              v-for="ingredient in selectedMenuIngredients || []" :key="ingredient">
-              <!-- <img
+            <div class="description px-12 py-2">
+              <p class="text-2xl font-bold text-gray-600">Description</p>
+              <p class="font-noto-thai font-medium text-gray-600 text-lg indent-6 ps-4 py-2">
+              <p class="font-noto-thai font-medium text-gray-600 text-lg indent-6 ps-4 py-2">
+                {{
+                  selectedMenu?.description ||
+                  'Achieve a 5-star rating to collect all of these menus!'
+                }}
+              </p>
+            </div>
+            <div class="ingredients px-12 py-2">
+              <p class="text-2xl font-bold text-gray-600">Ingredients</p>
+              <div class="container flex flex-row justify-items-center gap-4 m-4 w-auto h-auto overflow-x-auto">
+                <div class="flex flex-col gap-2 mb-4 px-2 py-2 rounded-lg min-w-20 bg-gray-200" v-if="!selectedMenu"
+                  v-for="n in 8" :key="n">
+                  <img :src="selectedMenu
+                    ? `/meat/${selectedMenu?.ingredients}.png`
+                    : '/unknownIngredient.png'
+                    " :alt="selectedMenu?.ingredients || 'unknown ingredient'" />
+                  <p class="text-sm text-center">Unknown Ingredient</p>
+                </div>
+                <div class="flex flex-col gap-2 mb-4 px-2 py-2 rounded-lg min-w-20 bg-gray-200"
+                  v-for="ingredient in selectedMenuIngredients || []" :key="ingredient">
+                  <div class="flex flex-col gap-2 mb-4 px-2 py-2 rounded-lg min-w-20 bg-gray-200"
+                    v-for="ingredient in selectedMenuIngredients || []" :key="ingredient">
+                    <!-- <img
                 src="/public/meat/redEyedSnakeEggs.png"
                 alt="red-eyed-snake-eggs"
               /> -->
-              <img :src="`/${ingredient.type}/${ingredient.name}.png`"
-                :alt="ingredient.display_name || 'unknown ingredient'" />
-              <p class="text-sm text-center">
-                {{ ingredient.display_name }} ({{ ingredient.amount }})
-              </p>
+                    <img :src="`/${ingredient.type}/${ingredient.name}.png`"
+                      :alt="ingredient.display_name || 'unknown ingredient'" />
+                    <p class="text-sm text-center">
+                      {{ ingredient.display_name }} ({{ ingredient.amount }})
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
 </template>
 
 <style scoped>
--- src/components/IngredientBar.vue
+++ src/components/IngredientBar.vue
@@ -46,10 +46,14 @@ watchEffect(() => {
     vegetables.value = userVegetables
 })
 
+let clickCount = ref(0) // Track the number of ingredient clicks
 const handleIngredientClick = async (targetIngredient) => {
 
+    if (clickCount.value >= 14) return
+    clickCount.value++
+    console.log(`Click Count: ${clickCount.value}`)
+
     if (targetIngredient.amount < 1) return
-    if (gameState.cauldron.length >= 14) return
 
     const updateIngredients = userStore.user.userDetail.ingredients.map((ingd) => {
         return {
@@ -85,49 +89,55 @@ const handleToggleFoodStoreClick = () => {
             class="pointer-events-none w-full h-16 absolute bottom-0 bg-[linear-gradient(180deg,_rgba(0,0,0,0)_0%,_rgba(0,0,0,0.5)_100%)]">
         </div>
         <div class="flex-none bg-base text-center text-xl font-rowdies p-2 ">
-            <p class="py-3">Ingredient</p>
-            <div class="flex  rounded-lg">
-                <div @click="handleSelectPage(0)"
-                    class="flex-1 flex justify-center cursor-pointer bg-[#c3e0c1] hover:bg-[#90a58e] rounded-lg border border-white">
-                    <img src="/meat.png" alt="meat-bar" class="w-12">
-                </div>
-                <div @click="handleSelectPage(1)"
-                    class="flex-1 flex justify-center cursor-pointer bg-[#c3e0c1] hover:bg-[#90a58e] rounded-lg border border-white ">
-                    <img src="/vegetable.png" alt="vegetable-bar" class="w-12">
+            <div class="flex-none bg-base text-center text-xl font-rowdies p-2 ">
+                <p class="py-3">Ingredient</p>
+                <div class="flex  rounded-lg">
+                    <div @click="handleSelectPage(0)"
+                        class="flex-1 flex justify-center cursor-pointer bg-[#c3e0c1] hover:bg-[#90a58e] rounded-lg border border-white">
+                        <img src="/meat.png" alt="meat-bar" class="w-12">
+                    </div>
+                    <div @click="handleSelectPage(1)"
+                        class="flex-1 flex justify-center cursor-pointer bg-[#c3e0c1] hover:bg-[#90a58e] rounded-lg border border-white ">
+                        class="flex-1 flex justify-center cursor-pointer bg-[#c3e0c1] hover:bg-[#90a58e] rounded-lg
+                        border border-white ">
+                        <img src="/vegetable.png" alt="vegetable-bar" class="w-12">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div
-            class="flex-auto bg-zinc-700 p-2 flex flex-col items-center gap-2 max-h-[28rem] overflow-y-auto custom-scrollbar">
-            <button @click="handleIngredientClick(meat)" v-show="selectedPage === 0" v-for="meat in meats"
-                :key="meat.id" class="bg-white hover:bg-gray-300 hover:border-4 border-[#77628C] transition-[border]
+            <div
+                class="flex-auto bg-zinc-700 p-2 flex flex-col items-center gap-2 max-h-[28rem] overflow-y-auto custom-scrollbar">
+                <button @click="handleIngredientClick(meat)" v-show="selectedPage === 0" v-for="meat in meats" <button
+                    @click="handleIngredientClick(meat)" v-show="selectedPage === 0" v-for="meat in meats"
+                    :key="meat.id" class="bg-white hover:bg-gray-300 hover:border-4 border-[#77628C] transition-[border]
             cursor-pointer rounded-lg w-10/12 h-20 flex" :class="{ 'grayscale': meat.amount === 0 }">
-                <div class="bg-secondary-100 text-white p-1 rounded-full h-[40%] flex items-center justify-between border-2
+                    cursor-pointer rounded-lg w-10/12 h-20 flex" :class="{ 'grayscale': meat.amount === 0 }">
+                    <div class="bg-secondary-100 text-white p-1 rounded-full h-[40%] flex items-center justify-between border-2
             border-secondary-200">
-                    {{ meat.amount }}
-                </div>
-                <div class="flex">
-                    <img :src="`/meat/${meat.name}.png`" :alt="meat.name" class="w-16" />
-                    <span class="font-serif h-7 px-3 rounded-lg">{{
-                        meat.display_name
-                        }}</span>
-                </div>
-            </button>
-            <div v-show="selectedPage === 1" v-for="vegetable in vegetables" :key="vegetable.id" class="bg-white hover:bg-gray-300 hover:border-4 border-[#77628C] transition-[border]
+                        {{ meat.amount }}
+                    </div>
+                    <div class="flex">
+                        <img :src="`/meat/${meat.name}.png`" :alt="meat.name" class="w-16" />
+                        <span class="font-serif h-7 px-3 rounded-lg">{{
+                            meat.display_name
+                            }}</span>
+                    </div>
+                </button>
+                </button>
+                <div v-show="selectedPage === 1" v-for="vegetable in vegetables" :key="vegetable.id" class="bg-white hover:bg-gray-300 hover:border-4 border-[#77628C] transition-[border]
             cursor-pointer rounded-lg w-10/12 h-20 flex" @click="handleIngredientClick(vegetable)">
-                <div class="bg-secondary-100 text-white p-1 rounded-full h-[40%] flex justify-center items-center border-2
+                    <div class="bg-secondary-100 text-white p-1 rounded-full h-[40%] flex justify-center items-center border-2
             border-secondary-200">
-                    {{ vegetable.amount }}
-                </div>
-                <div class="flex">
-                    <img :src="`/vegetable/${vegetable.name}.png`" :alt="vegetable.name" class="w-16" />
-                    <span class="font-serif h-7 px-3 rounded-lg">{{
-                        vegetable.display_name
-                        }}</span>
+                        {{ vegetable.amount }}
+                    </div>
+                    <div class="flex">
+                        <img :src="`/vegetable/${vegetable.name}.png`" :alt="vegetable.name" class="w-16" />
+                        <span class="font-serif h-7 px-3 rounded-lg">{{
+                            vegetable.display_name
+                            }}</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 </template>
 
